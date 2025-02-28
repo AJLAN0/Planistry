b285a64b2e937d355ad44815390ab8cf74785d5a
AJLAN0 <aj0ly.hd@gmail.com>
2025-02-28 20:46:11 +0300

    .

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,16 @@
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from products.models import Product
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['descibe'] = model_data.describ
-        data['price'] = model_data.price
+        data = model_to_dict(model_data)
 
-    return JsonResponse(data)
+
+    return Response(data)
