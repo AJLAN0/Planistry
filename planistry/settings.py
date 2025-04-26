8381112672378ff8df608edfbeff02af2705a573
AJLAN0 <aj0ly.hd@gmail.com>
2025-04-26 09:22:28 +0300

    add logging to project

--- planistry/settings.py
+++ planistry/settings.py
@@ -113,8 +113,16 @@ WSGI_APPLICATION = 'planistry.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),  # بنخليه يقرأ من Environment Variables
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Password validation
